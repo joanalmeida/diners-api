c5b25f5f755bb040e0e70faa916116ca01dda036
Nicolas <nicolas.morel@darwoft.com>
2023-08-07 16:46:32 -0300

    Feature, case insensitve para products

--- src/products/application/domain/product.types.ts
+++ src/products/application/domain/product.types.ts
@@ -6,6 +6,7 @@ export type ProductQuery = {
   [ProductProp in keyof Product]?: {
     operator: Operator;
     value: Product[ProductProp];
+    mode: string;
     // [K in Operator]?: Product[ProductProp];
   };
 };
